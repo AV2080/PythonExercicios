798a68df63fc6d4eaec804f74ac9a529029d0446
Alexandre Vitale Alves <123398521+AV2080@users.noreply.github.com>
2023-04-05 11:00:38 -0300

    Update ex096.py

--- ex096.py
+++ ex096.py
@@ -1,6 +1,6 @@
 def área(larg, comp):
     a = larg * comp
-    print(f'A área de um terreno  {larg}x{comp} é de {a}m2')
+    print(f'A área de um terreno  {larg}x{comp} é de {a}m²') #ALT+0178
 
 
 # Programa Principal
