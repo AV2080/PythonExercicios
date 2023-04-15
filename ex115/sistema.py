b47ac2b30462a14c121c0643244cd05936ac0692
Alexandre Vitale Alves <123398521+AV2080@users.noreply.github.com>
2023-04-15 14:18:44 -0300

    Atualizei ex115

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -10,10 +10,14 @@ if not arquivoExiste(arq):
 while True:
     resposta = menu(['Ver Pessoas Cadastradas', 'Cadastrar Nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-       # Opção de listar o conteudo de um arquivo
+       # Opção de listar o conteudo de um arquivo.
         lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('Opção  2')
+        # Opção de cadastrar uma nova pessoa.
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaint('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabeçalho('Saindo do sistema...Até logo!')
         break
